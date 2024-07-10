2678f6e082b081babf2d67f754e7ef782b6960a4
Shobana V <159698530+Shobana1310@users.noreply.github.com>
2024-07-10 11:09:05 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-set -e 
+# setup.sh
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
 export ACCEPT_EULA=Y
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 apt-get install -y msodbcsql18
