ea0a7902565fde6c85274e6018b8ecf807754480
Shobana V <159698530+Shobana1310@users.noreply.github.com>
2024-07-10 12:59:21 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# setup.sh
-#!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status
-export ACCEPT_EULA=Y
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get install -y msodbcsql18
